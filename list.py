d6113a831d430081401a27f9d38f709c2f7bac68
Mahammad881 <mh5676023@gmail.com>
2025-07-21 15:39:12 +0530

    Update list.py

--- list.py
+++ list.py
@@ -1,2 +1,8 @@
 print("python is a high level language")
 ljwhvkwgit 
+'ods,fpodmfdjibhsdgysfnsklfS'
+'fmsfjzsiofjszfjsmfsomfsfm sknffnuszfhou
+'nsiddh8SAHd\fsofsidfj'
+'fmsiofjsuhfushfshfjmdoa'
+'ngvef5gcvygrgfe nmkyiejjmi'
+'. ju hy g hu j k jbb y i jni mni k '     mu vnj yg ty t v vgv gbhb v    cvx   '
